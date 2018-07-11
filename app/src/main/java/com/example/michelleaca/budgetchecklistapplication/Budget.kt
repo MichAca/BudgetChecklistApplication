745277a220a4c755f59638fb32de284ea797256f
MichAca <m.aca@outlook.ph>
2018-07-11 10:54:01 +0800

    modified xml file of budget activity

--- app/src/main/java/com/example/michelleaca/budgetchecklistapplication/Budget.kt
+++ app/src/main/java/com/example/michelleaca/budgetchecklistapplication/Budget.kt
@@ -2,11 +2,16 @@ package com.example.michelleaca.budgetchecklistapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_budget.*
 
 class Budget : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_budget)
+
+        add_btn.setOnClickListener{
+
+        }
     }
 }
--- app/src/main/java/com/example/michelleaca/budgetchecklistapplication/MainActivity.kt
+++ app/src/main/java/com/example/michelleaca/budgetchecklistapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,10 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        budget_Btn.setOnClickListener{
+            val budgetInt = Intent(this, Budget::class.java)
+            startActivity(budgetInt)
+        }
+
+        checkList_Btn.setOnClickListener{
+            val checklistInt = Intent(this, Checklist::class.java)
+            startActivity(checklistInt)
+        }
+
+
+
     }
 
-    fun checkList (view: View) {
-        val checkListIntent = Intent(this, Checklist::class.java)
-        startActivity(checkListIntent)
-    }
+
 }
